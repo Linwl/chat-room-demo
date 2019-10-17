729a7904b4b8c1181166aa03f8bc6aabed2bff96
林万龙 <304115325@qq.com>
2019-10-17 15:08:46 +0800

    初始化项目

---
+++ src/main/java/ChatRoomApplication.java
@@ -0,0 +1,13 @@
+/**
+ * @author ：linwl
+ * @date ：Created in 2019/10/17 15:06
+ * @description ：
+ * @modified By：
+ */
+public class ChatRoomApplication {
+
+    public static void main(String[] args)
+    {
+
+    }
+}
