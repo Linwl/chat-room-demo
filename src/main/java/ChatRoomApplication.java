8fe7d14f1bad958d71fb395fa5caa85553e8c3f3
林万龙 <304115325@qq.com>
2019-10-17 16:15:28 +0800

    增加nio服务端代码

--- src/main/java/ChatRoomApplication.java
+++ src/main/java/ChatRoomApplication.java
@@ -1,3 +1,5 @@
+import com.linwl.chatroomdemo.service.ChatServer;
+
 /**
  * @author ：linwl
  * @date ：Created in 2019/10/17 15:06
@@ -8,6 +10,6 @@ public class ChatRoomApplication {
 
     public static void main(String[] args)
     {
-
+        new ChatServer(8888).start();
     }
 }
